6f48bd21a011e3ba4ad01f95dd1f2d016ce4d1f8
YaneliTo <erickmme2@gmail.com>
2023-09-21 10:28:57 -0600

    Mi primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//import axios from 'axios';
+import firebase from 'firebase/app'; 
+import'firebase/database';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +12,22 @@ export class HomePage {
   direccion:any;
   ciudad:any;
   colonia:any;
+  nombre:any;
+  apellido:any;
 
   constructor() {}
-
+  async guardarFormulario(){
+    const databaseRef = firebase.database().ref('/formulario');
+    const datos = {
+      direccion: this.direccion,
+      nombre: this.nombre,
+      ciudad: this.ciudad,
+      colonia: this.colonia,
+      apellido:this.apellido,
+    }
+    databaseRef.child(this.nombre).set(datos).then(()=>{
+    console.log("Subido Exitosamente!");
+  })
+    
+  }
 }
